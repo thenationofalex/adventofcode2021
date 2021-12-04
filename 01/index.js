5e91134fd057a9ecf0110bea4ee06fc0ba5f0816
Alex B <alexb@thenationofalex.com>
2021-12-04 15:24:51 +1030

    day 1 - part 1

---
+++ 01/index.js
@@ -0,0 +1,29 @@
+// --- Day 1: Sonar Sweep ---
+const fs = require('fs')
+const path = require('path')
+
+fs.readFile(path.join(__dirname, 'input'), "utf8", (err, data) => {
+  if (err) {
+    throw err
+  }
+  
+  let answer = 1
+  const mapData = data.split('\n')
+
+  for (let index = 0; index < mapData.length; index++) {
+    const element = mapData[index];
+
+    if (index === 0) {
+      console.log(element, "(N/A - no previous measurement)")
+    } else if (element > mapData[index - 1]) {
+      console.log(element, "(increased)")
+      answer += 1
+    } else if ((element < mapData[index - 1])) {
+      console.log(element, "(decreased)")
+    }
+
+  }
+
+  console.log(answer)
+  return answer
+})
