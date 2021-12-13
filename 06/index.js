6179d342bb366ebd7c5dde3126741b24fd290f6c
thenationofalex <best.alexander@gmail.com>
2021-12-13 13:44:10 +1100

    Update index.js
    refactor to fix memory leak

--- 06/index.js
+++ 06/index.js
@@ -3,26 +3,30 @@ const fs = require('fs')
 class Sea {
     
     constructor() {
-        this.school = []
+        this.school = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     }
 
     addFishToSea (fish) {
-        this.school.push(fish)
+        this.school[fish]++
     }
 
     getAllTheFish () {
-        return this.school.length
+        return this.school.reduce((a,b) => a + b)
     }
 
-    age() {
-        this.school.forEach(fish => {
-            if (fish.age === 0) {
-                fish.age = 6
-                this.addFishToSea({ age: 8 })
-            } else {
-                fish.age -= 1
-            }
-        })
+    age(days) {
+        for (let i = 0; i < days; i++) {
+            let [d0, d1, d2, d3, d4, d5, d6, d7, d8] = this.school
+            this.school[0] = d1
+            this.school[1] = d2
+            this.school[2] = d3
+            this.school[3] = d4
+            this.school[4] = d5
+            this.school[5] = d6
+            this.school[6] = d7 + d0
+            this.school[7] = d8
+            this.school[8] = d0
+        }
     }
 }
 
@@ -31,22 +35,19 @@ fs.readFile('input', (err, data) => {
     if (err) {
       throw err
     }
-    const lines = data.toString().trim().split(',')
+    const lines = data
+        .toString()
+        .trim()
+        .split(',')
+        .map(x => parseInt(x))
     
     const theSea = new Sea()
 
     lines.forEach(line => {
-        theSea.addFishToSea({ age: parseInt(line) })
+        theSea.addFishToSea(line)
     })
 
-    const timeLine = 30
-    let counter = 0
-
-    while (counter !== timeLine) {
-        theSea.age()
-        counter += 1
-    }
-
+    theSea.age(256)
     console.log(theSea.getAllTheFish())
 
 })
